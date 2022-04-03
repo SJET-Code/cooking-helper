237daa97cc6e7a2331f7f97ac7aa40b2abd48cb0
Santeri Tolonen <santeri.tolonen@helsinki.fi>
2022-04-03 16:18:28 +0300

    edit schema.sql and new.html

--- schema.sql
+++ schema.sql
@@ -1,10 +1,15 @@
-CREATE TABLE recepies (
+CREATE TABLE recipes (
     id SERIAL PRIMARY KEY,
     name TEXT
 );
+CREATE TABLE instructions (
+    id SERIAL PRIMARY KEY,
+    recipe_id INTEGER REFERENCES recipes,
+    instruction TEXT
+);
 CREATE TABLE ingredients (
     id SERIAL PRIMARY KEY,
-    recepie_id INTEGER REFERENCES recepies,
+    recipe_id INTEGER REFERENCES recipes,
     ingredient TEXT,
-    amount INTEGER
+    amount TEXT
 );
