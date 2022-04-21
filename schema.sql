dae91e25689a4741a9f99ac7a6b48cbfbc795108
Santeri Tolonen <santeri.tolonen@helsinki.fi>
2022-04-21 19:39:42 +0300

    add new tables and alter for cascade delete

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
+DROP TABLE users CASCADE;
+DROP TABLE recipes CASCADE;
+DROP TABLE instructions CASCADE;
+DROP TABLE ingredients CASCADE;
+DROP TABLE likes CASCADE;
+DROP TABLE recipe_list CASCADE;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
 CREATE TABLE recipes (
     id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
     name TEXT
 );
 CREATE TABLE instructions (
@@ -13,3 +26,51 @@ CREATE TABLE ingredients (
     ingredient TEXT,
     amount TEXT
 );
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    recipe_id INTEGER REFERENCES recipes,
+    user_id INTEGER REFERENCES users
+);
+CREATE TABLE cooking_plans (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    name TEXT
+);
+CREATE TABLE cp_recipes (
+    id SERIAL PRIMARY KEY,
+    cp_id INTEGER REFERENCES cooking_plans,
+    recipe_id INTEGER REFERENCES recipes
+);
+CREATE TABLE cp_hidden (
+    id SERIAL PRIMARY KEY,
+    cp_id INTEGER REFERENCES cooking_plans,
+    ingredient_id INTEGER REFERENCES ingredients
+);
+ALTER TABLE instructions
+DROP CONSTRAINT instructions_recipe_id_fkey, 
+ADD CONSTRAINT instructions_recipe_id_fkey
+FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE;
+ALTER TABLE ingredients
+DROP CONSTRAINT ingredients_recipe_id_fkey, 
+ADD CONSTRAINT ingredients_recipe_id_fkey
+FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE;
+ALTER TABLE cp_recipes
+DROP CONSTRAINT cp_recipes_recipe_id_fkey,
+ADD CONSTRAINT cp_recipes_recipe_id_fkey
+FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE;
+ALTER TABLE likes
+DROP CONSTRAINT likes_recipe_id_fkey,
+ADD CONSTRAINT likes_recipe_id_fkey
+FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE;
+ALTER TABLE cp_recipes
+DROP CONSTRAINT cp_recipes_cp_id_fkey,
+ADD CONSTRAINT cp_recipes_cp_id_fkey
+FOREIGN KEY (cp_id) REFERENCES cooking_plans (id) ON DELETE CASCADE;
+ALTER TABLE cp_hidden
+DROP CONSTRAINT cp_hidden_cp_id_fkey,
+ADD CONSTRAINT cp_hidden_cp_id_fkey
+FOREIGN KEY (cp_id) REFERENCES cooking_plans (id) ON DELETE CASCADE;
+ALTER TABLE cp_hidden
+DROP CONSTRAINT cp_hidden_ingredient_id_fkey,
+ADD CONSTRAINT cp_hidden_ingredient_id_fkey
+FOREIGN KEY (ingredient_id) REFERENCES ingredients (id) ON DELETE CASCADE;
